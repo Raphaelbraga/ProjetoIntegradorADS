ee10ef5f9007a299ae330e11bc4d5248687419a2
Vinicius França <viniciusfranca9@hotmail.com>
2022-04-13 19:40:17 -0300

    atualizacao tela de cadastro

--- src/br/com/LeituraAgua/DAO/HidrometroDAO.java
+++ src/br/com/LeituraAgua/DAO/HidrometroDAO.java
@@ -1,32 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package br.com.LeituraAgua.DAO;
-
-import br.com.model.Hidrometro;
-import static br.com.LeituraAgua.DAO.ConexaoDAO.connect;
-import static br.com.LeituraAgua.DAO.ConexaoDAO.stmt;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-/**
- *
- * @author Usuario
- */
-public class HidrometroDAO {
-
-    public boolean cadastrar(Hidrometro obj) {
-        boolean retorno = false;
-        try {
-            stmt = connect.createStatement();
-            String mysql = "INSERT INTO hidrometro id_hidrometro,leitura_instalacao"
-                    + " situacao, id_consumidor, id_endereco";
-
-            stmt.execute(mysql);
-        } catch (SQLException add) {
-            add.getMessage();
-        }
-        return false;
-    }
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package br.com.LeituraAgua.DAO;
+//
+//import br.com.model.Hidrometro;
+//import static br.com.LeituraAgua.DAO.ConexaoDAO.connect;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//
+///**
+// *
+// * @author Usuario
+// */
+//public class HidrometroDAO {
+//
+//    public boolean cadastrar(Hidrometro obj) {
+//        boolean retorno = false;
+//        try {
+//            stmt = connect.createStatement();
+//            String mysql = "INSERT INTO hidrometro id_hidrometro,leitura_instalacao"
+//                    + " situacao, id_consumidor, id_endereco";
+//
+//            stmt.execute(mysql);
+//        } catch (SQLException add) {
+//            add.getMessage();
+//        }
+//        return false;
+//    }
+//}
