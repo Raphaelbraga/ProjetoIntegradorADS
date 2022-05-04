919a3ac1560a26b32778752bb731e41d780bd008
Vinicius França <viniciusfranca9@hotmail.com>
2022-05-04 20:20:22 -0300

    atualizacao tela principal

--- src/br/com/LeituraAgua/Telas/TelaPrincipal.java
+++ src/br/com/LeituraAgua/Telas/TelaPrincipal.java
@@ -142,9 +142,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
